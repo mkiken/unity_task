fa53314e389fc277f95361233f922272c6231ed6
mkiken <mkiken0@gmail.com>
2017-02-12 03:59:26 +0900

    カード画面

---
+++ unity_task/Assets/scripts/CardController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardController : MonoBehaviour {
+	public Image cardImagePrefab;
+	public GameObject cardContent;
+
+	// Use this for initialization
+	void Start () {
+		Texture2D texture = Resources.Load("cards/c03") as Texture2D;
+		Image card = Instantiate(cardImagePrefab);
+		card.sprite = Sprite.Create(texture, new Rect(0,0,200,300), Vector2.zero);
+		card.transform.SetParent(cardContent.transform);
+	}
+}
