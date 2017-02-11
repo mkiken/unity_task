5daab966b40c314811e759f10efeeb93628839f1
mkiken <mkiken0@gmail.com>
2017-02-11 23:23:07 +0900

    チャットで送信できるようにした

---
+++ unity_task/Assets/scripts/ChatController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatController : MonoBehaviour {
+	public InputField inputField;
+	public GameObject chatContents;
+    public Text prefabChatText;
+
+	public void onSubmitClick(){
+		Text newText = Instantiate(prefabChatText);
+		newText.text = inputField.text;
+		newText.transform.SetParent(chatContents.transform);
+		inputField.text = "";
+
+	}
+
+}
