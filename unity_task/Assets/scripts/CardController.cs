2fc4020b7f411480ba9f698ef2f00aa5b1ffa24c
mkiken <mkiken0@gmail.com>
2017-02-12 04:26:31 +0900

    カードを並べた

--- unity_task/Assets/scripts/CardController.cs
+++ unity_task/Assets/scripts/CardController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CardController : MonoBehaviour {
-	public Image cardImagePrefab;
-	public GameObject cardContent;
+public class CardController : MonoBehaviour
+{
+    public Image cardImagePrefab;
+    public GameObject cardContent;
 
-	// Use this for initialization
-	void Start () {
-		Texture2D texture = Resources.Load("cards/c03") as Texture2D;
-		Image card = Instantiate(cardImagePrefab);
-		card.sprite = Sprite.Create(texture, new Rect(0,0,200,300), Vector2.zero);
-		card.transform.SetParent(cardContent.transform);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // Texture2D texture = Resources.Load("cards/c03") as Texture2D;
+        // Image card = Instantiate(cardImagePrefab);
+        // card.sprite = Sprite.Create(texture, new Rect(0,0,200,300), Vector2.zero);
+        // card.transform.SetParent(cardContent.transform);
+        Object[] textures = Resources.LoadAll("cards");
+        foreach (Object obj in textures)
+        {
+			Texture2D texture = obj as Texture2D;
+			if (texture == null){
+				continue;
+			}
+            Image card = Instantiate(cardImagePrefab);
+            card.sprite = Sprite.Create(texture as Texture2D, new Rect(0, 0, 200, 300), Vector2.zero);
+            card.transform.SetParent(cardContent.transform);
+        }
+    }
 }
