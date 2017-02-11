201aa8edf9826ab317dbc61127c0629fefce8f71
mkiken <mkiken0@gmail.com>
2017-02-11 18:12:15 +0900

    タブを押してポップアップを出せるようにした

---
+++ unity_task/Assets/scripts/PopupController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupController : MonoBehaviour
+{
+    public static GameObject prefab;
+    // ビューを表示するstaticメソッド
+    public static PopupController Show(){
+        if (prefab == null){
+            prefab = Resources.Load("Chat View") as GameObject;
+        }
+
+        // プレハブをインスタンス化してビューを表示
+        GameObject obj = Instantiate(prefab) as GameObject;
+        PopupController popupView = obj.GetComponent<PopupController>();
+
+        return popupView;
+    }
+
+    // alertビューを閉じるメソッド
+    public void Dismiss(){
+        Destroy(gameObject);
+    }
+}
---
+++ unity_task/Assets/scripts/TabController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabController : MonoBehaviour {
+	public void OnPressChatTab(){
+		PopupController.Show();
+	}
+
+}
