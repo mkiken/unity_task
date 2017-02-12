4efd9b4d978ae995a1cba0285e6822065477ea5d
mkiken <mkiken0@gmail.com>
2017-02-13 02:31:39 +0900

    ガチャ結果画面

--- unity_task/Assets/scripts/GachaDrawController.cs
+++ unity_task/Assets/scripts/GachaDrawController.cs
@@ -2,35 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GachaDrawController : MonoBehaviour {
-	public Image cardImagePrefab;
+public class GachaDrawController : MonoBehaviour
+{
+    public Image cardImagePrefab;
     public GameObject gachaResult;
 
     // Use this for initialization
     void Start()
     {
-		List<Texture2D> textureList = new List<Texture2D>();
+        List<Texture2D> textureList = new List<Texture2D>();
         Object[] textures = Resources.LoadAll("cards");
         foreach (Object obj in textures)
         {
-			Texture2D texture = obj as Texture2D;
-			// 多分metaファイル？を弾く
-			if (texture == null){
-				continue;
-			}
-			textureList.Add(texture);
+            Texture2D texture = obj as Texture2D;
+            // 多分metaファイル？を弾く
+            if (texture == null)
+            {
+                continue;
+            }
+            textureList.Add(texture);
         }
 
-		// 有効なTextureの中からランダムで1つ選ぶ
+        // 有効なTextureの中からランダムで1つ選ぶ
         Texture2D tex = textureList[Random.Range(0, textureList.Count)];
         Image card = Instantiate(cardImagePrefab);
-            card.sprite = Sprite.Create(tex, new Rect(0, 0, 200, 300), Vector2.zero);
-            card.transform.SetParent(gachaResult.transform);
+        card.sprite = Sprite.Create(tex, new Rect(0, 0, 200, 300), Vector2.zero);
+        card.transform.SetParent(gachaResult.transform);
+
+		// 5秒後にシーン移動
+		Invoke("goGachaScene", 5.0f);
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void goGachaScene()
+    {
+        SceneManager.LoadScene("Gacha");
+    }
+
 }
