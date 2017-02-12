d522cdfba34dc396f428422312bb642eb51c110c
mkiken <mkiken0@gmail.com>
2017-02-13 02:21:42 +0900

    ガチャランダム選出

---
+++ unity_task/Assets/scripts/GachaDrawController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GachaDrawController : MonoBehaviour {
+	public Image cardImagePrefab;
+    public GameObject gachaResult;
+
+    // Use this for initialization
+    void Start()
+    {
+		List<Texture2D> textureList = new List<Texture2D>();
+        Object[] textures = Resources.LoadAll("cards");
+        foreach (Object obj in textures)
+        {
+			Texture2D texture = obj as Texture2D;
+			// 多分metaファイル？を弾く
+			if (texture == null){
+				continue;
+			}
+			textureList.Add(texture);
+        }
+
+		// 有効なTextureの中からランダムで1つ選ぶ
+        Texture2D tex = textureList[Random.Range(0, textureList.Count)];
+        Image card = Instantiate(cardImagePrefab);
+            card.sprite = Sprite.Create(tex, new Rect(0, 0, 200, 300), Vector2.zero);
+            card.transform.SetParent(gachaResult.transform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
