8cf2dd68b6f14f1a56e5f68af44397a3c5aadeb1
mkiken <mkiken0@gmail.com>
2017-02-13 20:00:30 +0900

    HPゲージ作成

---
+++ unity_task/Assets/scripts/HpBarController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		setRandomValue();
+		InvokeRepeating("setRandomValue", 1.0f, 3.0f);
+	}
+
+	void setRandomValue(){
+		Slider slider = GetComponent<Slider>();
+		slider.value = Random.Range(slider.minValue, slider.maxValue);
+	}
+}
