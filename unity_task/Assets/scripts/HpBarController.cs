467f443317c28fa9b1d961e2b2c43ff8e5a78579
mkiken <mkiken0@gmail.com>
2017-02-13 20:18:08 +0900

    iTweenでゲージを動かす

--- unity_task/Assets/scripts/HpBarController.cs
+++ unity_task/Assets/scripts/HpBarController.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HpBarController : MonoBehaviour {
+public class HpBarController : MonoBehaviour
+{
+    private Slider slider = null;
+    // Use this for initialization
+    void Start()
+    {
+        getSlider();
+        InvokeRepeating("setRandomValue", 1.0f, 5.0f);
+    }
 
-	// Use this for initialization
-	void Start () {
-		setRandomValue();
-		InvokeRepeating("setRandomValue", 1.0f, 3.0f);
-	}
+    void setRandomValue()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+    "from", slider.value,
+    "to", Random.Range(slider.minValue, slider.maxValue),
+    "time", 3.0f,
+    "easetype", iTween.EaseType.linear,
+    "onupdate", "setValue",
+    "onupdatetarget", gameObject
+));
+    }
 
-	void setRandomValue(){
-		Slider slider = GetComponent<Slider>();
-		slider.value = Random.Range(slider.minValue, slider.maxValue);
-	}
+    void setValue(float val)
+    {
+        Slider slider = GetComponent<Slider>();
+        slider.value = val;
+    }
+
+    Slider getSlider()
+    {
+        if (slider == null)
+        {
+            slider = GetComponent<Slider>();
+        }
+
+        return slider;
+    }
 }
