14b4aa7fbd9d9f773d19aee8a924c2158cdcd43c
mkiken <mkiken0@gmail.com>
2017-02-13 23:45:09 +0900

    chatはぎーにやってもらった

---
+++ unity_task/Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+public RectTransform rect;
+public RectTransform newrect;
+	// Use this for initialization
+	void Update () {
+		newrect.sizeDelta= rect.sizeDelta;
+	}
+	
+}
